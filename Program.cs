cd22cf07f1c4ff8ecb80a4b41cb5c1d4dcfe15b6
Amysoum <amysoumare189@gmail.com>
2025-12-06 22:10:26 +0000

    Premier commit - projet calculatrice

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Demande à l'utilisateur d'entrer un nombre
+        Console.Write("Entrez un nombre : ");
+
+        // Convertit la saisie (texte) en entier
+        int n = int.Parse(Console.ReadLine());
+
+        // Boucle de 1 à 10 pour afficher la table
+        for (int i = 1; i <= 10; i++)
+        {
+            // Affiche chaque ligne de la table
+            Console.WriteLine($"{n} x {i} = {n * i}");
+        }
+    }
+}
